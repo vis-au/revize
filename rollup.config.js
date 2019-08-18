ba4b8b950a891f552821997d51449fcd54236ca6
Marius Hogräfer <s0020279@mail.zih.tu-dresden.de>
2019-08-18 13:48:51 +0200

    add rollup configuration for bundled distribution of remodel

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
+export function disallowedImports() {
+  return {
+    resolveId: module => {
+      if (module === 'vega' || module === 'util') {
+        throw new Error('Cannot import from Vega or Node Util in Vega-Lite.');
+      }
+      return null;
+    }
+  };
+}
+
+export default {
+  input: 'dist/index.js',
+  output: {
+    file: 'build/remodel.js',
+    format: 'umd',
+    sourcemap: true,
+    name: 'vl'
+  },
+  plugins: [disallowedImports(), nodeResolve({browser: true}), commonjs(), json(), sourcemaps()]
+};
